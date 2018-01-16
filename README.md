3d616885c55d385c8bc9448bf018d59c9891253b
cactushead <cactushead@gmail.com>
2018-01-16 22:05:10 +0000

    updating

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ A Theme Manager for [MantisBT](http://www.mantisbt.org/), the free web-based bug
 
 2012 by http://tim-pietrusky.de
 2017 updated by Samunosuke
+2018 updated by Cactushead
 
 ## Version
 
-0.0.2 (20170420)
-Tested with Mantis 2.3 and 2.9
+0.0.3 (20180116)
+Tested with Mantis 2.9
 
 ## In Action
 
@@ -16,18 +17,40 @@ Tested with Mantis 2.3 and 2.9
 
 ## Install
 
-1. [Download](https://github.com/Samunosuke/MantisThemeManager/zipball/master)
+1. [Download](https://github.com/cactushead/MantisThemeManager/archive/master.zip)
 2. Unpack
-3. Rename the unpacked folder `TimPietrusky-MantisThemeManager-xxxxxxx` to `MantisThemeManager`
+3. Rename the unpacked folder called `TimPietrusky-MantisThemeManager-xxxxxxx` to `MantisThemeManager`
 4. Copy the `MantisThemeManager` folder to `<mantis-root>/plugins/`
 5. Go to *Manage / Manage Plugins* and hit the *Install* action for **MantisBT Theme Manager**
 6. Click on the **MantisBT Theme Manager x.x.x** link to open the *choose a theme* page
+7. Create a folder called `themes` inside `<mantis-root>/css/` if it does not yet exist
+8. Copy `<mantis-root>/plugins/MantisThemeManager/pages/default.png` to `<mantis-root>/css/themes/default/default.png`
 
 ## How to use
 
 Follow the **Instructions** on the **MantisBT Theme Manager** page.
 
 ## Themes
+
+### Cactus-Light-Theme
+- Creates a flat clean and vibrant theme
+- Alters side menu
+- Adds theme info to footer
+- No additional javascripts required
+
+<img src="cactus light.png" height="200" alt="cactus light theme for MantisBT">
+
+[Download](https://github.com/Samunosuke/mantis-suke-theme/zipball/master)
+
+
+### MantisBT is a ☆ ✪ ★ rockstar
+
+<img src="http://github.com/TimPietrusky/mantisbt-is-a-rockstar/raw/master/rockstar/rockstar.png" height="200" alt="rockstar theme for MantisBT">
+
+[Download](http://github.com/TimPietrusky/mantisbt-is-a-rockstar/zipball/master)
+
+### &lt;Your theme&gt;
+
 
 ### Mantis-Suke-Theme
 - adds gradient to bug list rows
@@ -37,21 +60,18 @@ Follow the **Instructions** on the **MantisBT Theme Manager** page.
 
 [Download](https://github.com/Samunosuke/mantis-suke-theme/zipball/master)
 
-
-### &lt;Your theme&gt;
-
 Please create a pull request or issue or what ever to add your theme to the list.
 
 #### How to submit a theme
 
 1. Your theme should have a unique name like `mytheme`
-2. Name your css `default.css` and drop it under `mytheme/default.css`
-3. Create a preview image of your theme, name it `mytheme.png` and drop it under `mytheme/mytheme.png`
+2. Name your css `default.css` and save it to `themes/mytheme/default.css`
+3. Create a preview image of your theme, name it `mytheme.png` and save it to `themes/mytheme/mytheme.png`
 4. Create what ever subfolders you like (e. g. `mytheme/img` for images used in your `default.css`)
 
 ## ToDo
 
- - Add more themes
+ - Create more themes
 
 
 ## License
--- pages/config.php
+++ pages/config.php
@@ -2,37 +2,29 @@
 /* edited by Samunosuke to meet MantisBT 2.x api changes */
 auth_reauthenticate( );
 access_ensure_global_level(config_get('manage_plugin_threshold'));
-
 layout_page_header(lang_get('plugin_themeManager_title'));
 layout_page_begin();
-
 print_manage_menu();
-
 // Variables
 $error = false;
-
 // Load available themes
 $themes = getThemes($error);
-
 ?>
 
 <style type="text/css">
-    .mantisbt-theme-manager .theme-name-active {font-weight:bold;}
+	/*Theme Manager specific css*/
+    .theme-name-active { font-weight: bold; }
 
-    .mantisbt-theme-manager img {
-        width:200px;
-        border:3px solid #fff;
-
-        overflow-y:hidden;
-
-        -webkit-border-radius:5px;
-           -moz-border-radius:5px;
-                border-radius:5px;
-
+    #themelist img {
+        width: 200px;
+        border: 3px solid #fff;
+        overflow-y: hidden;
+        -webkit-border-radius: 3px;
+           -moz-border-radius: 3px;
+                border-radius: 3px;
         -webkit-box-shadow: 2px 2px 10px #777;
            -moz-box-shadow: 2px 2px 10px #777;
                 box-shadow: 2px 2px 10px #777;
-
         -webkit-transition: all .3s ease-in-out;
            -moz-transition: all .3s ease-in-out;
             -ms-transition: all .3s ease-in-out;
@@ -40,92 +32,172 @@ $themes = getThemes($error);
                 transition: all .3s ease-in-out;
     }
 
-    .mantisbt-theme-manager img:hover {
+    #themelist img:hover {
         -webkit-transform: scale(1.8);
            -moz-transform: scale(1.8);
             -ms-transform: scale(1.8);
              -o-transform: scale(1.8);
                 transform: scale(1.8);
     }
-}
+
+	.widget-title {
+		font-size: 1.71428571em !important;
+		font-style: inherit !important;
+		font-weight: 500 !important;
+		letter-spacing: -.01em !important;
+		line-height: 1.16666667 !important;
+		margin-top: 28px !important;
+		color: #172B4D !important;
+	}
+
+	.widget-header { color: #0052CC !important; }
+
+	.badge,
+	.label {
+		font-weight: 400 !important;
+		background-color: #42526E !important;
+		text-shadow: none !important;
+	}
+
+	.label.arrowed-in:before,
+	.label.arrowed:before {
+		border-right-color: #42526E !important;
+	}
+
+	.widget-color-blue2 { border-color: #fff !important;	}
+
+	.table-bordered,
+	.table-bordered>tbody>tr>td,
+	.table-bordered>tbody>tr>th,
+	.table-bordered>tfoot>tr>td,
+	.table-bordered>tfoot>tr>th,
+	.table-bordered>thead>tr>td,
+	.table-bordered>thead>tr>th {
+		border: 0px !important;
+	}
+
+	.table-striped>tbody>tr:nth-of-type(odd) { background-color: #fff !important; }
+
+	.widget-body .table.table-bordered thead:first-child>tr {
+		border-top: 0px !important;
+		border-bottom: 2px solid #DFE1E6 !important;
+	}
+
+	.widget-title .badge { background-color: #ABBAC3 !important; }
+
+	.widget-color-blue2>.widget-header {
+		background: #fff !important;
+		border-color: #fff !important;
+	}
+
+	#themelist>tbody>tr:last-child { border-bottom: 2px solid #DFE1E6 !important; }
+
+	.negative,
+	.positive {
+		color: #fff;
+		padding: 0 10px;
+		margin-left: 5px;
+	}
+
+	.negative { background-color: #dc3c36; }
+	.positive { background-color: #27ae60; }
+
+	.row-1 td,
+	.row-2 td {
+		padding: 20px !important;
+	}
+
+	.row-1 img,
+	.row-2 img {
+		width: 200px;
+	}
+
+	.row-category td {
+		text-align: center !important;
+		color: #42526E !important;
+	}
+
+	.form-title {
+		font-size: 20px;
+		padding-bottom: 20px !important;
+		text-align: center !important;
+	}
+
+	td.category, th.category { background-color: #fff !important; }
 </style>
 
 <br />
-<form action="<?php echo plugin_page( 'config_edit' )?>" method="post">
-    <table align="center" class="width50 mantisbt-theme-manager" cellspacing="1">
 
-<!-- Title -->
-    <tr>
-        <td>
-            <table align="center" class="width100 mantisbt-theme-manager" cellspacing="1">
-                <tr>
-                	<td class="form-title" colspan="3">
-                		<?php echo lang_get( 'plugin_themeManager_title' ); ?>
-                	</td>
-                </tr>
-            </table>
-        </td>
-    </tr>
+<form id="bug_action" action="<?php echo plugin_page( 'config_edit' )?>" method="post">
+
+	<div class="widget-box widget-color-blue2">
+		<div class="widget-header widget-header-small">
+			<div class="widget-body">
+
+				<!-- Title -->
+				<h4 class="widget-title lighter">
+					<i class="ace-icon fa fa-columns"></i>
+						<?php echo lang_get( 'plugin_themeManager_title' ); ?>
+					<span class="badge">
+						<?php echo lang_get( 'plugin_themeManager_config' )?>
+					</span>
+				</h4>
+
+				<!-- Choose Theme -->
+				<table id="themelist" class="table table-striped table-bordered table-condensed table-hover" style="width:600px;">
+					<thead>
+						<tr>
+							<th class="category" style="width:250px;"><?php echo lang_get('plugin_themeManager_category_row_theme'); ?></th>
+							<th class="category" style="width:50px;"><?php echo lang_get('plugin_themeManager_category_row_selected'); ?></th>
+							<th class="category" style="width:250px; padding-left: 20px;"><?php echo lang_get('plugin_themeManager_category_row_preview'); ?></th>
+							<th class="category" style="width:50px;"></th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<?php foreach ($themes as $theme): ?>
+							<tr <?php echo helper_alternate_class()?>>
+								<td class="row-category center theme-name<?php if ($theme['isActive']) {echo "-active";} ?>" >
+									<?php echo $theme['name']; ?>
+								</td>
+								<td class="center" >
+									<label>
+										<input
+											type="radio"
+											name="active_theme"
+											value="<?php echo $theme['name'];?>"
+											<?php if ($theme['isActive']) { ?>
+												checked="checked"
+											<?php } ?>
+										/>
+
+										<?php if ($theme['isActive']): ?>
+											<span class="required"></span>
+										<?php endif; ?>
+									</label>
+								</td>
+								<td class="right">
+									<a href="<?php echo $theme['img']; ?>">
+										<img src="<?php echo $theme['img']; ?>" alt="<?php echo $theme['name'];?>" title="<?php echo lang_get('plugin_themeManager_preview'); ?>">
+									</a>
+								</td>
+								<td style="vertical-align: middle; z-index: -100;">
+									<?php if ($theme['isActive']): ?>
+										<span class="label hidden-xs label-default arrowed">active theme</span>
+									<?php endif; ?>
+								</td>
+							</tr>
+						<?php endforeach ?>
+					</tbody>
+
+				</table>
+
+				<input tabindex="22" type="submit" class="btn btn-primary btn-white btn-round" value="<?php echo lang_get( 'plugin_themeManager_choose' )?>">
 
 
-<!-- Choose Theme -->
-    <tr>
-        <td>
-            <table align="center" class="width100 mantisbt-theme-manager" cellspacing="1">
-
-            <tr>
-            	<td class="form-title" colspan="3">
-            		<?php echo lang_get( 'plugin_themeManager_config' )?>
-            	</td>
-            </tr>
-
-            <tr class="row-category">
-            	<td><?php echo lang_get('plugin_themeManager_category_row_theme'); ?></td>
-            	<td><?php echo lang_get('plugin_themeManager_category_row_selected'); ?></td>
-            	<td><?php echo lang_get('plugin_themeManager_category_row_preview'); ?></td>
-            </tr>
-
-            <?php foreach ($themes as $theme): ?>
-                <tr <?php echo helper_alternate_class()?>>
-            	<td class="row-category center theme-name<?php if ($theme['isActive']) {echo "-active";} ?>" width="20%">
-            		<?php echo $theme['name']; ?>
-            	</td>
-            	<td class="center" width="40%">
-            		<label>
-            		    <input
-            		        type="radio"
-            		        name="active_theme"
-            		        value="<?php echo $theme['name'];?>"
-            		        <?php if ($theme['isActive']): ?>
-                                checked="checked"
-            		        <?php endif; ?>
-            		    />
-
-            		    <?php if ($theme['isActive']): ?>
-            		        <span class="required"></span>
-            		    <?php endif; ?>
-            		</label>
-            	</td>
-            	<td class="right" width="40%">
-            	    <a href="<?php echo $theme['img']; ?>">
-            		    <img src="<?php echo $theme['img']; ?>" alt="<?php echo $theme['name'];?>" title="<?php echo lang_get('plugin_themeManager_preview'); ?>">
-            		</a>
-            	</td>
-            </tr>
-            <?php endforeach ?>
-
-            </table>
-        </td>
-    </tr>
-
-
-    <tr>
-    	<td class="center" colspan="3">
-    		<input type="submit" class="button" value="<?php echo lang_get( 'plugin_themeManager_choose' )?>" />
-    	</td>
-    </tr>
-
-    </table>
+			</div>
+		</div>
+	</div>
 </form>
 
 <?php
@@ -135,11 +207,7 @@ $themes = getThemes($error);
 
 <?php
 layout_page_end();
-
-
-
 /* functions */
-
 /**
  * Get all local themes
  */
@@ -147,27 +215,23 @@ function getThemes(&$error) {
     $active_theme = plugin_config_get('active_theme');
     $themes_path = getcwd() . "/css/themes/";
     $themes = array();
-
     // Add default theme
     $themes[] = array(
         'name' => 'default',
-        'img' => "plugins/MantisThemeManager/pages/default.png",
+        'img' => "./css/themes/default/default.png",
         'isActive' => $active_theme == 'default'
     );
-
-
     // Load themes from css/themes
     if (file_exists($themes_path)) {
         if ($handle = opendir($themes_path)) {
             while (false !== ($theme_name = readdir($handle))) {
-                if ($theme_name !== '.' && $theme_name !== '..') {
+                if ($theme_name !== '.' && $theme_name !== '..' && $theme_name !== 'default') {
                     $themes[] = array(
                         'name' => $theme_name,
-                        'img' => "../css/themes/$theme_name/" . $theme_name . ".png",
+                        'img' => "./css/themes/$theme_name/" . $theme_name . ".png",
                         'isActive' => $active_theme == $theme_name
                     );
                 }
-
             }
             closedir($handle);
         }
@@ -176,6 +240,5 @@ function getThemes(&$error) {
         // Bullshit, I know
         $error = true;
     }
-
     return $themes;
 }
